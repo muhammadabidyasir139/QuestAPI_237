26c42f41bd3faf5da5ca9113b034208a2a6fa363
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-12-18 14:13:34 +0700

    feat: implement Navigation Graph and HostNavigasi

--- app/src/main/java/com/example/localapi/uicontroller/DataSiswaApp.kt
+++ app/src/main/java/com/example/localapi/uicontroller/DataSiswaApp.kt
@@ -9,10 +9,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.localapi.ui.view.siswa.EntrySiswaScreen
 
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.localapi.ui.view.siswa.DestinasiEntry
+import com.example.localapi.ui.view.siswa.EntrySiswaScreen
+import com.example.localapi.ui.view.siswa.HomeScreen
+import com.example.localapi.uicontroller.route.DestinasiHome
+
 @Composable
-fun DataSiswaApp(modifier: Modifier = Modifier) {
-    EntrySiswaScreen(
-        navigateBack = {},
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController, modifier = modifier)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
         modifier = modifier
-    )
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { /* Navigate to detail */ }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
 }
