c2a5e74260c34609c1fec3311557a6753b2517a5
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-12-18 13:56:02 +0700

    feat: add addSiswa to EntryViewModel

--- app/src/main/java/com/example/localapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localapi/viewmodel/EntryViewModel.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.localapi.modeldata.DetailSiswa
 import com.example.localapi.modeldata.UIStateSiswa
+import com.example.localapi.modeldata.toDataSiswa
 import com.example.localapi.repositori.RepositoryDataSiswa
+import retrofit2.Response
+import kotlinx.coroutines.launch
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
@@ -25,4 +29,17 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): View
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
+    /* Fungsi untuk menyimpan data yang di-entry */
+    fun addSiswa() {
+        viewModelScope.launch {
+            if (validasiInput()) {
+                val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+                if (sip.isSuccessful){
+                    println("Sukses Tambah Data : ${sip.message()}")
+                }else{
+                    println("Gagal tambah data : ${sip.errorBody()}")
+                }
+            }
+        }
+    }
 }
