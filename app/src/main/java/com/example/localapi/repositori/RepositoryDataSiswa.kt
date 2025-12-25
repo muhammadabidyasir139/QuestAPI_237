47470950c3455b50de80d93bd582fb42cf71d0ee
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2025-12-25 14:05:09 +0700

    Update RepositoryDataSiswa.kt

--- app/src/main/java/com/example/localapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localapi/repositori/RepositoryDataSiswa.kt
@@ -5,6 +5,9 @@ import com.example.localapi.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun updateDataSiswa(id: String, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun deleteDataSiswa(id: String): retrofit2.Response<Void>
+    suspend fun getSiswaById(id: String): DataSiswa
 }
 
 class JaringanRepositoryDataSiswa(
@@ -12,4 +15,7 @@ class JaringanRepositoryDataSiswa(
 ) : RepositoryDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun updateDataSiswa(id: String, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.updateSiswa(id, dataSiswa)
+    override suspend fun deleteDataSiswa(id: String): retrofit2.Response<Void> = serviceApiSiswa.deleteSiswa(id)
+    override suspend fun getSiswaById(id: String): DataSiswa = serviceApiSiswa.getSiswaById(id)
 }
